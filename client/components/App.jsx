9d6bb81f0041cabe6903bcfdda20c08171137f41
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-01 15:53:17 +1300

    createBoard func generates random colours to be used as candy based off gameboard size

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,33 @@ import React from 'react'
 
 function App () {
 
+  const width = 8
+  const candyColours = [
+    'red',
+    'green',
+    'blue',
+    'orange',
+    'purple',
+    'yellow',
+  ]
+
+  // Fills array with random arrangement of colours from candyColours to size of game board.
+  const createBoard = () => {
+    const randomColourArrangement = []
+      for (let i = 0; i < width * width; i++){
+        const randomColour = candyColours[Math.floor(Math.random() * candyColours.length)]
+        randomColourArrangement.push(randomColour)
+      }
+      console.log(randomColourArrangement)
+  }
+
+  createBoard()
+
+
+
   return (
-    <>
-      <div className='app'>
-        🍬🔨
-      </div>
-    </>
+      <div className='app'> 
+      </div> 
   )
 }
 
